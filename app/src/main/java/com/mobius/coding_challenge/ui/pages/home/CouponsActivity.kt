9f2dc84b40d309349a2d6f293a4d2340f5297106
mirza <mirzaahmed65@gmail.com>
2021-03-13 08:57:09 +0530

    removed wildcard imports

--- app/src/main/java/com/mobius/coding_challenge/ui/pages/home/CouponsActivity.kt
+++ app/src/main/java/com/mobius/coding_challenge/ui/pages/home/CouponsActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mobius.coding_challenge.ui.pages.home
 
 import android.os.Bundle
@@ -33,19 +48,26 @@ class CouponsActivity : BaseVMActivity<CouponsViewModel, ActivityCouponsBinding>
     }
 
     private fun observeCoupons() {
-        viewModel.couponResponse.observe(this, Observer {
-            it.checkResponse(onSuccess = {
-                binding.progressView.visibility = View.GONE
-                it.value.takeIf { it.isNotEmpty() }?.let { loadCouponList(it) } ?: Toast.makeText(
-                    this@CouponsActivity,
-                    "No data found",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }, onError = {
-                binding.progressView.visibility = View.GONE
-                handleError(it)
-            })
-        })
+        viewModel.couponResponse.observe(
+            this,
+            Observer {
+                it.checkResponse(
+                    onSuccess = {
+                        binding.progressView.visibility = View.GONE
+                        it.value.takeIf { it.isNotEmpty() }?.let { loadCouponList(it) }
+                            ?: Toast.makeText(
+                                this@CouponsActivity,
+                                "No data found",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                    },
+                    onError = {
+                        binding.progressView.visibility = View.GONE
+                        handleError(it)
+                    }
+                )
+            }
+        )
     }
 
     private fun loadCouponList(data: List<Coupon>) {
